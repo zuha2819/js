49dde39006b345ffa71468d8b7432db894f0bcf8
zuhriddin <juraevdev@gmail.com>
2022-10-29 18:38:48 +0500

    js

---
+++ script.js
@@ -0,0 +1,14 @@
+var name='ok bosing'
+alert(name)
+var userName =prompt('Ismingizni kiriting')
+
+alert('Salom  senga  ' +userName)
+
+var userYear = prompt('Yilingizni kiriting ? ')
+var hozir = (2022)
+alert('Sizning ismingiz ' + userName + '. Sizning yoshingiz ' +  (hozir - userYear) + ' da')
+
+alert('Hisoblang')
+var e = +prompt ('19+21')
+e = 40
+alert( 'Togri javob = 40' )
